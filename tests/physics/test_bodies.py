92643242fd788737e2255611a579d681c86a34a8
bluePlatinum <jukic.rok@gmail.com>
2022-05-29 01:49:35 +0200

    added basic functionality for Rigidbody

---
+++ tests/physics/test_bodies.py
@@ -0,0 +1,13 @@
+import AMMP.physics.bodies as bodies
+
+import numpy as np
+import random
+
+
+def test_rigidbody_constructor():
+    position = np.array([random.random() for i in range(3)])
+    rotation = np.array([random.random() for i in range(3)])
+    body = bodies.Rigidbody(position, rotation)
+
+    assert body.position.all() == position.all()
+    assert body.rotation.all() == rotation.all()
