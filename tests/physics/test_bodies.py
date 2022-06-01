9ccd912b13660b38203bf0bf00df7ce3f060c3cf
bluePlatinum <jukic.rok@gmail.com>
2022-06-01 23:25:36 +0200

    added documentation to test

--- tests/physics/test_bodies.py
+++ tests/physics/test_bodies.py
@@ -1,10 +1,14 @@
-import ammp.physics.bodies as bodies
+from ammp.physics import bodies
 
 import random
 import numpy as np
 
 
 def test_rigidbody_constructor():
+    """
+    Test the constructor for the bodies class.
+    :return: None
+    """
     position = np.array([random.random() for i in range(3)])
     rotation = np.array([random.random() for i in range(3)])
     body = bodies.Rigidbody(position, rotation)
