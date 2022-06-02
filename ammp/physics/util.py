bc2fc6c6ed70f4e9409411cd9de73fbdffd4ed1f
bluePlatinum <jukic.rok@gmail.com>
2022-06-03 01:07:50 +0200

    added Quat class with basic functionality

---
+++ ammp/physics/util.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+class Quat:
+    """
+    Creates a normalized Quaternion of the form (x, y, z, Re). Mainly used for
+    rotation.
+
+    :param quat: A array representing the quaternion as (x, y, z, Re)
+    :type quat: numpy.ndarray
+    """
+    def __init__(self, quat):
+        """
+        Constructor Method
+
+        :param quat: A array representing the quaternion as (x, y, z, Re)
+        :type quat: numpy.ndarray
+        """
+        quat = quat / np.linalg.norm(quat)  # normalize the vector
+        self._quat = quat
+
+    @classmethod
+    def from_angle_axis(cls, angle, axis):
+        """
+        Create a Quat object from an angle in radians and a rotation axis.
+
+        :param angle: The angle for the rotation
+        :type angle: number
+        :param axis: The axis of the rotation
+        :type axis: array or numpy.ndarray
+        :return: Quat object
+        :rtype: Quat
+        """
+        axis = axis / np.linalg.norm(axis)  # normalize the vector
+        quat = np.array([*(np.sin(angle) * axis), np.cos(angle)])
+        return cls(quat)
+
+    # TODO: create as_vector() method
+    # TODO: create as_angle_axis() method
