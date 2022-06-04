b1a7a897310f241416fac71c6017d287e7e724e7
bluePlatinum <jukic.rok@gmail.com>
2022-06-05 00:18:18 +0200

    added CartesianReference class with transform_to method

--- ammp/physics/reference.py
+++ ammp/physics/reference.py
@@ -22,3 +22,36 @@ class Reference:
         self.position = position
         self.rotation = rotation
         self.parent_reference = parent_reference
+
+
+class CartesianReference(Reference):
+    """
+    Creates a cartesian frame of reference.
+
+    :param position: The position of the reference expressed in vectors of
+        the root reference.
+    :type position: Numpy array
+    :param rotation: To rotation of the reference expressed
+    :tpye position: scipy rotation
+    :param parent_reference: The parent reference of the reference. This is the
+        frame of reference in which the position and rotation of this
+        Reference is defined.
+    """
+    def __init__(self, position, rotation, parent_reference):
+        """
+        Constructor method
+        """
+        super().__init__(position, rotation, parent_reference)
+
+    def transform_to(self, vector):
+        """
+        Transforms a vector expressed in the parent frame of reference to this
+        frame of reference
+
+        :param vector: a vector to be transformed
+        :type vector: numpy.ndarray
+        :return: transformed vector
+        :rtype: numpy.ndarray
+        """
+        inverse = self.rotation.inv()
+        return inverse.apply(vector - self.position)
--- tests/physics/test_reference.py
+++ tests/physics/test_reference.py
@@ -16,15 +16,80 @@ def root_reference():
     return ref
 
 
-def test_reference_constructor(root_reference):
+@pytest.fixture
+def system_set_static():
     """
-    Test the constructor of the physics.Reference class
-    :return: None
+    Creates a static system of references for testing.
     """
-    position = np.random.rand(3)
-    rotation = Rotation.from_matrix(np.random.rand(3, 3))
-    ref = reference.Reference(position, rotation, root_reference)
+    # Create root reference
+    position = np.array([0, 0, 0])
+    rotation = Rotation.from_matrix(np.identity(3))
+    root_ref = reference.CartesianReference(position, rotation, None)
 
-    assert ref.position.all() == position.all()
-    assert ref.rotation == rotation
-    assert ref.parent_reference == root_reference
+    # Create first reference
+    position = np.array([1, 2, 0])
+    rotation = Rotation.from_rotvec(np.pi/4 * np.array([1, 0, 0]))
+    first_ref = reference.CartesianReference(position, rotation, root_ref)
+
+    # Create second reference
+    position = np.array([0, 0, 3])
+    rotation = Rotation.from_rotvec(np.pi/2 * np.array([0, 0, 1]))
+    second_ref = reference.CartesianReference(position, rotation, root_ref)
+
+    # Create third reference
+    position = np.array([0, 0, 3])
+    rotation = Rotation.from_rotvec(np.pi/2 * np.array([0, 0, 1]))
+    third_ref = reference.CartesianReference(position, rotation, second_ref)
+
+    # Create fourth reference
+    position = np.array([0, 0, -3])
+    rotation = Rotation.from_rotvec(-np.pi/2 * np.array([0, 0, 1]))
+    fourth_ref = reference.CartesianReference(position, rotation, second_ref)
+
+    return root_ref, first_ref, second_ref, third_ref, fourth_ref
+
+
+class TestReference:
+    def test_constructor(self, root_reference):
+        """
+        Test the constructor of the physics.Reference class
+        :return: None
+        """
+        position = np.random.rand(3)
+        rotation = Rotation.from_matrix(np.random.rand(3, 3))
+        ref = reference.Reference(position, rotation, root_reference)
+
+        assert ref.position.all() == position.all()
+        assert ref.rotation == rotation
+        assert ref.parent_reference == root_reference
+
+
+class TestCartesianReference:
+    def test_constructor(self, root_reference):
+        """
+        Test the constructor of the physics.CartesianReference class
+        :return: None
+        """
+        position = np.random.rand(3)
+        rotation = Rotation.from_matrix(np.random.rand(3, 3))
+        ref = reference.Reference(position, rotation, root_reference)
+
+        assert ref.position.all() == position.all()
+        assert ref.rotation == rotation
+        assert ref.parent_reference == root_reference
+
+    def test_transform_to(self, system_set_static):
+        """
+        Test the CartesianReferenec.transform_to method with a static system.
+        """
+        references = system_set_static
+        vector = np.array([3, 4, 5])
+        expected_root = vector
+        expected_1 = np.array([2, 4.94974747, 2.12132034])
+        expected_2 = np.array([4, -3, 2])
+        expected_3 = np.array([4, -3, 2])
+
+        assert references[0].transform_to(vector).all() == expected_root.all()
+        assert references[1].transform_to(vector).all() == expected_1.all()
+        assert references[2].transform_to(vector).all() == expected_2.all()
+        assert references[3].transform_to(vector).all() == expected_3.all()
