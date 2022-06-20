3d4d9f27ba7755ee138ad61073a7f601aeebd5c1
bluePlatinum <jukic.rok@gmail.com>
2022-06-20 21:46:20 +0200

    added test for MainWidget class

---
+++ tests/ui/test_mainWidget.py
@@ -0,0 +1,23 @@
+from ammp.ui.mainWidget import MainWidget
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QWidget
+
+
+class TestMainWidget:
+    """
+    Tests for the MainWidget Class
+    """
+    def test_constructor(self):
+        """
+        Tests the constructor of MainWidget
+        """
+        app = QApplication(sys.argv)
+        parent = QWidget()
+        focus = QWidget()
+        widget = MainWidget(parent=parent)
+        widget.currentFocus = focus
+
+        assert widget.parent() == parent
+        assert widget.currentFocus == focus
