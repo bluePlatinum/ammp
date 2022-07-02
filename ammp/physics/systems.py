a5df866af44bc74a750fe5552ccbb8702db887b6
bluePlatinum <33969983+bluePlatinum@users.noreply.github.com>
2022-07-02 14:43:26 +0200

    Dev/system viewer graphics (#27)
    * created SystemViewerGraphic Widget to plot Systems
    
    * created System class
    
    * changed inheritance for SystemDisplay and created implemented draw_sc method
    
    * updated requirements.txt
    
    * created tests for System class
    
    * Fixed broken reqs (now should world for 3.7, 3.8, 3.9, 3.10) (#29)
    
    * Fixed broken reqs (now should world for 3.7, 3.8, 3.9, 3.10)
    
    * added python 3.10 to supported versions
    
    * added tox (#30)
    
    * updated python-package workflow for improved lint (#31)
    
    * created SystemViewerGraphic Widget to plot Systems
    
    * created System class
    
    * changed inheritance for SystemDisplay and created implemented draw_sc method
    
    * updated requirements.txt
    
    * created tests for System class
    
    * added pytest-qt for testing (#28)
    
    * added pytest-qt for testing
    
    * changed pytest to verbose
    
    * Revert "changed pytest to verbose"
    
    This reverts commit 996e0e7796912ca19b71f741eef4cd0f2f8bb160.
    
    * added configuration for pytest-qt
    
    * Fixed broken reqs (now should world for 3.7, 3.8, 3.9, 3.10) (#29)
    
    * Fixed broken reqs (now should world for 3.7, 3.8, 3.9, 3.10)
    
    * added python 3.10 to supported versions
    
    * added tox (#30)
    
    * updated python-package workflow for improved lint (#31)
    
    * created SystemViewerGraphic Widget to plot Systems
    
    * created System class
    
    * changed inheritance for SystemDisplay and created implemented draw_sc method
    
    * updated requirements.txt
    
    * created tests for System class
    
    * fix after rebase
    
    * changed pytest to verbose
    
    * Revert "changed pytest to verbose"
    
    This reverts commit 996e0e7796912ca19b71f741eef4cd0f2f8bb160.
    
    * added configuration for pytest-qt
    
    * added noqa for flake8
    
    * adapted github-workflow for Qt tests (#33)

--- ammp/physics/systems.py
+++ ammp/physics/systems.py
@@ -1,6 +1,31 @@
+import random
 
 
-class SystemDisplay:
+class System:
+    """
+    The parent class for all systems.
+    """
+    def __init__(self):
+        """
+        Constructor method
+        """
+        pass
+
+    def draw_sv(self, sv_graphic):
+        """
+        This method draws the system for the SystemViewerGraphic QWidget.
+        It needs to be specifically implemented by the child classes.
+
+
+        :param sv_graphic: the SystemViewerGrapic QWidget
+        :type sv_graphic: SystemViewerGraphic
+        :return: None
+        """
+        msg = 'The draw_sv method was not implemented by the child class.'
+        raise NotImplementedError(msg)
+
+
+class SystemDisplay(System):
     """
     Creates a System intended for displaying static and semistatic
     constellations.
@@ -10,4 +35,27 @@ class SystemDisplay:
     :type bodies: list
     """
     def __init__(self, bodies):
+        super().__init__()
         self.bodies = bodies
+
+    def draw_sv(self, sv_graphic):
+        """
+        Draws the system for the SystemViewerGraphic widget by plotting every
+        body with its position and size.
+
+        :param sv_graphic: the SystemViewerGraphic widget
+        :type sv_graphic: SystemViewerGraphic
+        :return: None
+        """
+        for body in self.bodies:
+            # there is no scaling between physical size and display size
+            # implemented so this will definitely change, but for now it is
+            # a placeholder
+            try:
+                size = body.size
+            except AttributeError:
+                size = 1
+
+            color = (random.random(), random.random(), random.random())
+            sv_graphic.axes.plot(*body.position, marker='o', markersize=size,
+                                 color=color)
